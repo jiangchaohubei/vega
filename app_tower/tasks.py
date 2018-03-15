886f90c05eedfd5b8685aee14707926c88731f66
jiangchao_hubei <jc199310081121>
2018-03-15 10:32:02 +0800

    jc-执行攻击页面统计

--- app_tower/tasks.py
+++ app_tower/tasks.py
@@ -4,11 +4,11 @@ from celery import task
 from billiard.exceptions import Terminated#方便更好的取消任务
 import time
 #ansible2.4  API
-# from app_tower.utils.playbook_run2_4 import runplaybook
-# from app_tower.utils.commands_run2_4 import commandsrun
+from app_tower.utils.playbook_run2_4 import runplaybook
+from app_tower.utils.commands_run2_4 import commandsrun
 #ansible2.3  API
-from app_tower.utils.playbook_run import runplaybook
-from app_tower.utils.commands_run import commandsrun
+# from app_tower.utils.playbook_run import runplaybook
+# from app_tower.utils.commands_run import commandsrun
 
 from app_tower.models import T_JOB,T_Group,T_LOGIN_CREDENTIALS,T_JOB_EVENT,T_COMMAND_EVENT,sudo_record,T_JOB_TEMPLATE,T_TOOL_EVENT,T_TOOL_EVENT_COUNT
 from app_tower.utils.mygroup import mygroup
